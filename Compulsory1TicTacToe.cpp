ee796474fcfaf77e18296dc5d8eb7f2ddd259a71
Anders Petershagen Åsbø <39097620+FunkMarvel@users.noreply.github.com>
2022-01-22 14:58:57 +0100

    Initial commit

---
+++ Compulsory1TicTacToe.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Compulsory1TicTacToe.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Compulsory1TicTacToe, "Compulsory1TicTacToe" );
---
+++ Compulsory1TicTacToeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Compulsory1TicTacToeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COMPULSORY1TICTACTOE_API ACompulsory1TicTacToeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
