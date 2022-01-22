10325bdc65fbdc601dfd1494c7de0988d61920b4
Anders Petershagen Åsbø <39097620+FunkMarvel@users.noreply.github.com>
2022-01-22 15:29:07 +0100

    Removed duplicate files in wrong location

--- Compulsory1TicTacToe.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Compulsory1TicTacToe.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Compulsory1TicTacToe, "Compulsory1TicTacToe" );
