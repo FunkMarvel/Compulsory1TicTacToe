7458220a62ec2a152176e74d4f892929f81efe66
Anders Petershagen Åsbø <39097620+FunkMarvel@users.noreply.github.com>
2022-01-22 15:19:52 +0100

    Rebuilt project files

---
+++ Source/Compulsory1TicTacToe/Compulsory1TicTacToe.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Compulsory1TicTacToe.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Compulsory1TicTacToe, "Compulsory1TicTacToe" );
