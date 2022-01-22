98297459d2c2800e6295c20f8d72d99366db2afd
Anders Petershagen Åsbø <39097620+FunkMarvel@users.noreply.github.com>
2022-01-22 22:54:55 +0100

    Branch for testing input binding

---
+++ Source/Compulsory1TicTacToe/GameLogicComponent.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "GameLogicComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class COMPULSORY1TICTACTOE_API UGameLogicComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UGameLogicComponent();
+	// setup input
+	// setup gameboard
+	// on keypress iterate game state according to input
+	// track number of turns
+	// evaluate game over conditions
+	// take new input
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+		
+};
